0428e7d45faf844719b3220bb55eb692278c68b8
bootstrapcoding1 <bootstrapcoding1@gmail.com>
2025-10-19 15:24:14 +0400

    L1: Print a Report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from stats import get_num_words
 from stats import get_character_count
+from stats import dictionary_to_list
+from stats import sort_on
+from stats import print_dictionary
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -9,7 +12,11 @@ def get_book_text(file_path):
 def main():
     frankenstein_txt = get_book_text('books/frankenstein.txt')
     word_count = get_num_words(frankenstein_txt)
-    get_character_count(frankenstein_txt)
     print(f"Found {word_count} total words")
+    dictionary = get_character_count(frankenstein_txt)
+    print(dictionary)
+    list = dictionary_to_list(dictionary)
+    list.sort(reverse=True, key=sort_on)
+    print_dictionary(list)
 
 main()
--- stats.py
+++ stats.py
@@ -5,8 +5,35 @@ def get_num_words(string):
 def get_character_count(string):
     new_string = string.lower()
     character_set = set()
+    character_dict = dict([])
     for char in new_string:
         if char not in character_set:
             character_set.add(char)
+        if not (char in character_dict):
+            character_dict[char] = 1
+        else:
+            character_dict[char] += 1
+    return character_dict
 
-    return new_string
+def sort_on(items):
+    return items["num"]
+
+def  single_char_dictionary(char, number):
+    dictionary = {
+        "char": char,
+        "num": number
+    }
+    return dictionary
+
+def dictionary_to_list(dictionary):
+    list_of_chars = []
+    for item in dictionary:
+        list_of_chars.append(single_char_dictionary(item, dictionary[item]))
+    return list_of_chars
+
+def print_dictionary(list_of_chars):
+    for dict in list_of_chars:
+        if dict["char"].isalpha():
+            char = dict["char"]
+            num = dict["num"]
+            print(f"{char}: {num}")
